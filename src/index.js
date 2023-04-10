5e0f62dc6b35482e065c537a5c826c89db6e8877
W-Novytskyi <novickijvaceslav011@gmail.com>
2023-04-10 09:47:33 +0300

    структура

--- src/index.js
+++ src/index.js
@@ -6,31 +6,5 @@ import App from './App';
 createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+  </React.StrictMode>
 );
-
-// {
-// "username": "Jacques Gluke",
-// "tag": "jgluke",
-// "location": "Ocho Rios, Jamaica",
-// "avatar": "https://cdn-icons-png.flaticon.com/512/2922/2922506.png",
-// "stats": {
-//   "followers": 5603,
-//   "views": 4827,
-//   "likes": 1308
-// }
-// }
-
-// ReactDOM.render(
-//   <Profile
-//     username={user.username}
-//     tag={user.tag}
-//     location={user.locatioon}
-//     avatar={user.avatar}
-//     followers={user.stats.followers}
-//     views={user.stats.views}
-//     likes={user.stats.likes}
-//   />,
-//   document.getElementById('root')
-// );
