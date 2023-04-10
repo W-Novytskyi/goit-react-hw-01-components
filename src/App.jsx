ae5ddfab5ef2cbc0168505397e451c67ecdd276d
W-Novytskyi <novickijvaceslav011@gmail.com>
2023-04-10 08:18:32 +0300

    Перше завдання без стилів

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import Profile from 'components/Profile';
+import user from './user.json';
+
+export default function App() {
+  return (
+    <div>
+      <Profile
+        username={user.username}
+        tag={user.tag}
+        location={user.location}
+        avatar={user.avatar}
+        followers={user.stats.followers}
+        views={user.stats.views}
+        likes={user.stats.likes}
+      />
+    </div>
+  );
+}
--- src/index.js
+++ src/index.js
@@ -1,10 +1,36 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
-import { App } from 'components/App';
-import './index.css';
+import { createRoot } from 'react-dom/client';
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+import App from './App';
+
+createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <App />
-  </React.StrictMode>
+  </React.StrictMode>,
+  document.getElementById('root')
 );
+
+// {
+// "username": "Jacques Gluke",
+// "tag": "jgluke",
+// "location": "Ocho Rios, Jamaica",
+// "avatar": "https://cdn-icons-png.flaticon.com/512/2922/2922506.png",
+// "stats": {
+//   "followers": 5603,
+//   "views": 4827,
+//   "likes": 1308
+// }
+// }
+
+// ReactDOM.render(
+//   <Profile
+//     username={user.username}
+//     tag={user.tag}
+//     location={user.locatioon}
+//     avatar={user.avatar}
+//     followers={user.stats.followers}
+//     views={user.stats.views}
+//     likes={user.stats.likes}
+//   />,
+//   document.getElementById('root')
+// );
